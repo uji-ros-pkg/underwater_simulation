ad454c5aee54035c7718667dd24fa41c4f8a2f23
Yuri Gavshin <yuri.gavshin@gmail.com>
2013-10-26 12:40:52 +0300

    Fixed the "Assertion `!pthread_mutex_lock(m)' failed" error.

--- uwsim/src/SimulatedDevices.cpp
+++ uwsim/src/SimulatedDevices.cpp
@@ -17,49 +17,58 @@
 #include <pluginlib/class_loader.h>
 using namespace uwsim;
 
-pluginlib::ClassLoader<SimulatedDeviceFactory> simdev_loader("uwsim", "uwsim::SimulatedDeviceFactory");
+class SimulatedDevicesLoader{
+  boost::shared_ptr<pluginlib::ClassLoader<SimulatedDeviceFactory> >  simdev_loader;
+  vector<string> available_plugins;
+public:
+  //a list of "factories" to initialize and apply a device and/or rosinterface
+  std::vector<SimulatedDeviceFactory::Ptr> factories;
+    
+  SimulatedDevicesLoader(){
+    simdev_loader.reset( new pluginlib::ClassLoader<SimulatedDeviceFactory>("uwsim", "uwsim::SimulatedDeviceFactory") );
 
-//a list of "factories" to initialize and apply a device or rosinterface
-//an instance of "config" class is used as both a "factory" and as an XML structure
-std::vector<SimulatedDeviceFactory::Ptr> factories;
+    available_plugins = simdev_loader->getDeclaredClasses();
+    for (size_t i = 0; i< available_plugins.size();++i){
+      OSG_ALWAYS<<"Loading SimulatedDevices plugin: '"<<available_plugins.at(i)<<"'"<<std::endl;
+      factories.push_back(simdev_loader->createInstance(available_plugins.at(i)));
+    }
+    
+    for (size_t i = 0; i< factories.size();++i)
+      for (size_t j = 0; j< i;++j)
+        if (factories[i]->getType() == factories[j]->getType())
+          OSG_FATAL<<"SimulatedDevices factories types must be unique, but the same type '"<<factories[i]->getType()
+          <<"' is specified at indexes "<<j<<" and "<<i<<" in initFacotries() in SimulatedDevices.cpp" << std::endl;
+  }
+  
+  ~SimulatedDevicesLoader(){
+    factories.clear();
+    for (size_t i = 0; i< available_plugins.size();++i){
+      simdev_loader->unloadLibraryForClass(available_plugins.at(i));
+    }
+  }
+};
 
-//setting up a list of available devices/rosinterfaces
-static void initFactories(){
-	if (factories.size()>0) return;
-	
-	vector<string> available_plugins = simdev_loader.getDeclaredClasses();
-	for (size_t i = 0; i< available_plugins.size();++i){
-	  OSG_ALWAYS<<"Loading SimulatedDevices plugin: '"<<available_plugins.at(i)<<"'"<<std::endl;
-	  factories.push_back(simdev_loader.createInstance(available_plugins.at(i)));
-	}
-	
-	for (size_t i = 0; i< factories.size();++i)
-		for (size_t j = 0; j< i;++j)
-			if (factories[i]->getType() == factories[j]->getType())
-				OSG_FATAL<<"SimulatedDevices factories types must be unique, but the same type '"<<factories[i]->getType()
-				<<"' is specified at indexes "<<j<<" and "<<i<<" in initFacotries() in SimulatedDevices.cpp" << std::endl;
-}
+boost::shared_ptr<SimulatedDevicesLoader> loader(new SimulatedDevicesLoader());
 
 SimulatedDevices::SimulatedDevices(){}
 
 std::vector<boost::shared_ptr<ROSInterface> >
 SimulatedDevices::getInterfaces(ROSInterfaceInfo & rosInterface, std::vector<boost::shared_ptr<SimulatedIAUV> > & iauvFile){
-	initFactories();
-	std::vector<boost::shared_ptr<ROSInterface> > ifaces;
-	bool isFactoryFound = false;
-	if (rosInterface.type==ROSInterfaceInfo::SimulatedDevice){
-		for (size_t i = 0; i< factories.size();++i)
-			if (factories[i]->getType()==rosInterface.subtype){
-				isFactoryFound = true;
-				std::vector<boost::shared_ptr<ROSInterface> > ifaces_ = factories[i]->getInterface(rosInterface, iauvFile);
-				for (size_t j = 0; j< ifaces_.size();++j)
-					ifaces.push_back(ifaces_[j]);
-			}
-			
-		if(!isFactoryFound)
-			OSG_FATAL<<"Unknown ROSIterface '"<<rosInterface.subtype<<"ROS', skipping..."<<std::endl;
-	}
-	return ifaces;
+  std::vector<boost::shared_ptr<ROSInterface> > ifaces;
+  bool isFactoryFound = false;
+  if (rosInterface.type==ROSInterfaceInfo::SimulatedDevice){
+    for (size_t i = 0; i< loader->factories.size();++i)
+      if (loader->factories[i]->getType()==rosInterface.subtype){
+        isFactoryFound = true;
+        std::vector<boost::shared_ptr<ROSInterface> > ifaces_ = loader->factories[i]->getInterface(rosInterface, iauvFile);
+        for (size_t j = 0; j< ifaces_.size();++j)
+          ifaces.push_back(ifaces_[j]);
+      }
+      
+    if(!isFactoryFound)
+      OSG_FATAL<<"Unknown ROSIterface '"<<rosInterface.subtype<<"ROS', skipping..."<<std::endl;
+  }
+  return ifaces;
 }
 
 void SimulatedDevices::applyConfig(SimulatedIAUV * auv, Vehicle &vehicleChars, SceneBuilder *oscene)
@@ -67,8 +76,8 @@ void SimulatedDevices::applyConfig(SimulatedIAUV * auv, Vehicle &vehicleChars, S
   for (size_t iteration = 0; iteration< 10;++iteration)//executed max 10 times
   {
     bool configured = true;
-    for (size_t i = 0; i< factories.size();++i){
-      if(!factories[i]->applyConfig(auv, vehicleChars, oscene, iteration))
+    for (size_t i = 0; i< loader->factories.size();++i){
+      if(!loader->factories[i]->applyConfig(auv, vehicleChars, oscene, iteration))
 	configured = false;
     }
     if (configured) break;
@@ -77,50 +86,49 @@ void SimulatedDevices::applyConfig(SimulatedIAUV * auv, Vehicle &vehicleChars, S
 
 static void processConfigNode(const xmlpp::Node* node, ConfigFile * config, SimulatedDeviceConfig::Ptr cfg)
 {
-	if (!cfg) return;
-	
-	xmlpp::Node::NodeList list = node->get_children();
-	for(xmlpp::Node::NodeList::iterator iter = list.begin(); iter != list.end(); ++iter){
-		xmlpp::Node* child=dynamic_cast<const xmlpp::Node*>(*iter);
-		if (child->get_name()=="name" && cfg->name.length()==0)
-			config->extractStringChar(child, cfg->name);
-	}
+  if (!cfg) return;
+  
+  xmlpp::Node::NodeList list = node->get_children();
+  for(xmlpp::Node::NodeList::iterator iter = list.begin(); iter != list.end(); ++iter){
+    xmlpp::Node* child=dynamic_cast<const xmlpp::Node*>(*iter);
+    if (child->get_name()=="name" && cfg->name.length()==0)
+      config->extractStringChar(child, cfg->name);
+  }
 }
 
 std::vector< SimulatedDeviceConfig::Ptr >
 SimulatedDevices::processConfig(const xmlpp::Node* node, ConfigFile * config){
-	initFactories();
-	std::vector< SimulatedDeviceConfig::Ptr > devs;
-	xmlpp::Node::NodeList list = node->get_children();
-	for(xmlpp::Node::NodeList::iterator iter = list.begin(); iter != list.end(); ++iter){
-		xmlpp::Node* child=dynamic_cast<const xmlpp::Node*>(*iter);
-		
-		if (child->get_name()!="text"){
-			bool isFactoryFound = false;
-			for (size_t i = 0; i< factories.size();++i)
-				if (factories[i]->getType()==child->get_name()){
-					isFactoryFound = true;
-					SimulatedDeviceConfig::Ptr dev = factories[i]->processConfig(child, config);
-					if (dev)
-						processConfigNode(child, config, dev);
-					devs.push_back(dev);
-				}
-			if (!isFactoryFound)
-				OSG_FATAL<<"Unknown SimulatedDevice '"<<child->get_name()<<"', skipping..."<<std::endl;
-		}
-	}
-	return devs;
+  std::vector< SimulatedDeviceConfig::Ptr > devs;
+  xmlpp::Node::NodeList list = node->get_children();
+  for(xmlpp::Node::NodeList::iterator iter = list.begin(); iter != list.end(); ++iter){
+    xmlpp::Node* child=dynamic_cast<const xmlpp::Node*>(*iter);
+    
+    if (child->get_name()!="text" && child->get_name()!="comment"){
+      bool isFactoryFound = false;
+      for (size_t i = 0; i< loader->factories.size();++i)
+        if (loader->factories[i]->getType()==child->get_name()){
+          isFactoryFound = true;
+          SimulatedDeviceConfig::Ptr dev = loader->factories[i]->processConfig(child, config);
+          if (dev)
+            processConfigNode(child, config, dev);
+          devs.push_back(dev);
+        }
+      if (!isFactoryFound)
+        OSG_FATAL<<"Unknown SimulatedDevice '"<<child->get_name()<<"', skipping..."<<std::endl;
+    }
+  }
+  return devs;
 }
 
 SimulatedDeviceConfig::SimulatedDeviceConfig(std::string type){
-	this->type = type;
+  this->type = type;
 }
 
 SimulatedDeviceFactory::SimulatedDeviceFactory(std::string type){
-	this->type = type;
+  this->type = type;
 }
 
 SimulatedDevice::SimulatedDevice(SimulatedDeviceConfig * cfg){
-	this->type = cfg->getType();
-	this->name = cfg->name;
+  this->type = cfg->getType();
+  this->name = cfg->name;
 }
